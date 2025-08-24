172f90a3e5f5cb32065eff99b9d969ba6d7f0cfb
Benjamin Ellis <benjaminjellis@protonmail.com>
2025-08-24 14:01:53 +0100

    Update nvim, switch to jj

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,6 +1,42 @@
 -- git related plugins
 return {
   {
+    "nicolasgb/jj.nvim",
+    cmd = "J",
+    lazy = false,
+    config = function()
+      require("jj").setup({})
+      local cmd = require("jj.cmd")
+      vim.keymap.set("n", "<leader>jd", cmd.describe, { desc = "JJ describe" })
+      vim.keymap.set("n", "<leader>je", cmd.edit, { desc = "JJ edit" })
+      vim.keymap.set("n", "<leader>jn", cmd.new, { desc = "JJ new" })
+      vim.keymap.set("n", "<leader>js", cmd.status, { desc = "JJ status" })
+      vim.keymap.set("n", "<leader>dj", cmd.diff, { desc = "JJ diff" })
+      vim.keymap.set("n", "<leader>sj", cmd.squash, { desc = "JJ squash" })
+
+      -- Pickers
+      vim.keymap.set("n", "<leader>gj", function()
+        require("jj.picker").status()
+      end, { desc = "JJ Picker status" })
+      vim.keymap.set("n", "<leader>gl", function()
+        require("jj.picker").file_history()
+      end, { desc = "JJ Picker file history" })
+
+      -- Some functions like `describe` or `log` can take parameters
+      vim.keymap.set("n", "<leader>jl", function()
+        cmd.log({
+          revisions = "'all()'",
+        })
+      end, { desc = "JJ log" })
+
+      -- This is an alias i use for moving bookmarks its so good
+      vim.keymap.set("n", "<leader>jt", function()
+        cmd.j("tug")
+        cmd.log({})
+      end, { desc = "JJ tug" })
+    end,
+  },
+  {
     "pwntester/octo.nvim",
     cmd = "Octo",
     event = { { event = "BufReadCmd", pattern = "octo://*" } },
