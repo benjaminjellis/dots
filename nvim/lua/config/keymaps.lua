ad4cb9a920f73fd4bebecd434f4ec7ddade62506
benjamin <32769527+benjaminjellis@users.noreply.github.com>
2025-11-27 17:55:17 +0000

    fix: keymaps for search (#24)

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,2 +1,9 @@
 vim.keymap.set({ "n", "v" }, ";", ":", { remap = true })
 vim.keymap.set("n", "<leader>cb", "<cmd>%bd|e#<cr>")
+
+-- invert default set up for grep search and fuzzy file find
+vim.keymap.set("n", "<leader>sg", LazyVim.pick("live_grep", { desc = "Grep (workspace)", root = false }))
+vim.keymap.set("n", "<leader>sG", LazyVim.pick("live_grep", { desc = "Grep (crate)", root = true }))
+
+vim.keymap.set("n", "<leader>ff", LazyVim.pick("files", { desc = "Find files (workspace)", root = false }))
+vim.keymap.set("n", "<leader>fF", LazyVim.pick("files", { desc = "Find files (crate)", root = true }))
