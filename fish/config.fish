97ddb92845f8f0057d349833f6aa9e64a30be7bd
Benjamin Ellis <benjaminjellis@protonmail.com>
2026-03-02 16:04:27 +0100

    feat: add ocaml, remove clojure repl (#9)
    Reviewed-on: https://codeberg.org/benjaminjellis/dots/pulls/9
    Co-authored-by: Benjamin Ellis <benjaminjellis@protonmail.com>
    Co-committed-by: Benjamin Ellis <benjaminjellis@protonmail.com>

--- fish/config.fish
+++ fish/config.fish
@@ -43,3 +43,5 @@ function fish_greeting
         return
     end
 end
+
+test -r '/home/ben/.opam/opam-init/init.fish' && source '/home/ben/.opam/opam-init/init.fish' >/dev/null 2>/dev/null; or true
