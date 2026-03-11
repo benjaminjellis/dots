39bf8f850c2c75cc199b9b4036e156e36ce42896
Benjamin Ellis <benjaminjellis@protonmail.com>
2026-03-11 15:43:12 +0100

    feat: support mond (#12)
    Reviewed-on: https://codeberg.org/benjaminjellis/dots/pulls/12
    Co-authored-by: Benjamin Ellis <benjaminjellis@protonmail.com>
    Co-committed-by: Benjamin Ellis <benjaminjellis@protonmail.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,9 @@
+-- before anything register the .mond file extension
+vim.filetype.add({
+  extension = {
+    mond = "mond",
+  },
+})
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
---
+++ nvim/lua/plugins/mond.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "mond",
+        callback = function(args)
+          vim.bo[args.buf].commentstring = ";; %s"
+          pcall(vim.treesitter.start, args.buf, "mond")
+        end,
+      })
+      return opts
+    end,
+  },
+  -- turn off auto insert of () on accepting a completion
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        accept = {
+          auto_brackets = {
+            override_brackets_for_filetypes = {
+              mond = { "", "" },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        mond = {
+          cmd = { "mond", "lsp" },
+          filetypes = { "mond" },
+          root_markers = { "mond.toml", ".git" },
+        },
+      },
+    },
+  },
+}
